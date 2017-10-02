d5297e149a27d67ca88aa114155041d08107617a
Ankit Aggarwal <ankitaggarwal011@gmail.com>
2017-10-02 19:46:14 -0400

    Version 0.2.

--- mydump.cpp
+++ mydump.cpp
@@ -1,3 +1,12 @@
+/** 
+    
+    Author: Ankit Aggarwal
+    Description: mydump is a passive network monitoring application.
+    Build: sudo make clean && make
+    Usage: sudo ./mydump [-i interface] [-r file] [-s string] expression
+
+**/
+
 #include <iostream>
 #include <sstream>
 #include <cstdio>
@@ -6,8 +15,6 @@
 #include <ctime>
 #include <unistd.h>
 #include <pcap.h>
-#include <sys/types.h>
-#include <sys/socket.h>
 #include <netinet/in.h>
 #include <netinet/ether.h>
 #include <arpa/inet.h>
@@ -17,22 +24,8 @@
 #define IPV4 0x0800
 
 #define SIZE_ETHERNET 14
-#define IP_RF 0x8000        /* reserved fragment flag */
-#define IP_DF 0x4000        /* don't fragment flag */
-#define IP_MF 0x2000        /* more fragments flag */
-#define IP_OFFMASK 0x1fff   /* mask for fragmenting bits */
 #define IP_HL(ip)       (((ip)->ip_vhl) & 0x0f)
-#define IP_V(ip)        (((ip)->ip_vhl) >> 4)
 #define TH_OFF(th)  (((th)->th_offx2 & 0xf0) >> 4)
-#define TH_FIN 0x01
-#define TH_SYN 0x02
-#define TH_RST 0x04
-#define TH_PUSH 0x08
-#define TH_ACK 0x10
-#define TH_URG 0x20
-#define TH_ECE 0x40
-#define TH_CWR 0x80
-#define TH_FLAGS (TH_FIN|TH_SYN|TH_RST|TH_ACK|TH_URG|TH_ECE|TH_CWR)
 
 using namespace std;
 
